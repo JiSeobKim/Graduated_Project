60a1b3c8018f430371fa68fb2dd98ba61e8da38a
JiseobKim <jiseob812@gmail.com>
2017-09-27 18:12:57 +0900

    구성원 & DB 연결

--- Graduated_Project/Add/AddMember.swift
+++ Graduated_Project/Add/AddMember.swift
@@ -8,7 +8,51 @@
 
 import Foundation
 import UIKit
+import Alamofire
 
 class AddMember: UIViewController {
+    
+    //Nas
+    let URL_USER_REGISTER = "http://222.107.27.68:5123/swift/graduate/register.php"
+    
+    //View variables
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldPhone: UITextField!
+    @IBOutlet weak var textFieldCoin: UITextField!
+    @IBOutlet weak var labelStatus: UILabel!
+    
+    
+    
+    @IBAction func buttonRegist(_ sender: Any) {
+        //creating parameters for the post request
+        let parameters: Parameters=[
+            "name":textFieldName.text!,
+            "email":textFieldEmail.text!,
+            "phone":textFieldPhone.text!,
+            "coin":textFieldName.text!,
+            ]
+        
+        
+        //Sending http post request
+        Alamofire.request(URL_USER_REGISTER, method: .post, parameters: parameters , encoding: URLEncoding.httpBody).responseJSON
+            {
+                response in
+                //getting the json value from the server
+                if let result = response.result.value {
+                    
+                    let jsonData = result as! NSDictionary
+                    self.labelStatus.text = "상태 : \(jsonData["message"] as! String)"
+                    
+                }
+        }
+    }
+    //Button action
+    @IBAction func buttonRegister(_ sender: UIButton) {
+        
+        
+        
+    }
+    
     
 }
