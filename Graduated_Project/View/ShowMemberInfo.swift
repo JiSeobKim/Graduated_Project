1b1be6ecb3dd12fc762889ec669294903fe630dd
JiseobKim <jiseob812@gmail.com>
2017-09-29 19:14:21 +0900

    구성원 & 정보 표시 완료

--- Graduated_Project/View/ShowMemberInfo.swift
+++ Graduated_Project/View/ShowMemberInfo.swift
@@ -15,8 +15,20 @@ class ShowMemberInfo : UIViewController {
     var getValue : Member_Info?
     
     
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelPhone: UILabel!
+    @IBOutlet weak var labelEmail: UILabel!
+    @IBOutlet weak var labelCoin: UILabel!
+    
+    
     override func viewDidLoad() {
-        print(getValue?.name)
+        
+        if let data = getValue {
+            labelName.text = data.name
+            labelPhone.text = data.phone
+            labelEmail.text = data.email
+            labelCoin.text = data.coin
+        }
     }
     
 }
